567e9e60e6be6d42b823b15cc5d48de081c6735f
Andrej Maricic <andrejmaricicbiz@gmail.com>
2025-07-27 17:26:40 +0200

    Backup before clean design implementation - 2025-07-27 17:26

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
+
+@NgModule({
+  declarations: [
+    BottomNavComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    BottomNavComponent
+  ]
+})
+export class SharedModule { }
