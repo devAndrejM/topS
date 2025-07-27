8f9a118d24b8f764c9263f9447ef82f2ef1e33d3
Andrej Maricic <andrejmaricicbiz@gmail.com>
2025-07-27 22:41:34 +0200

    ok

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.clothingsearch.app',
-  appName: 'ClothingSearch.Frontend',
-  webDir: 'www'
-};
-
-export default config;
